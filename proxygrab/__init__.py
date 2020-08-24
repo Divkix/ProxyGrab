694c690cca37d4dd561ce255f4b230d991f4ffcd
Skuzzy xD <techdroidroot@gmail.com>
2020-08-24 03:46:49 +0000

    Revert "try foit"
    This reverts commit ef61720b422391dc2847cd33fb2a49a424040bf8.

--- proxygrab/__init__.py
+++ proxygrab/__init__.py
@@ -1,4 +1,1 @@
-"""A tool to scrap and fetch proxoes from various sites"""
-__version__ = "0.2.1"
-
 from .package import get_socks4, get_https, get_http, get_socks5, get_proxy
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import setuptools
+
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="ProxyGrab",
+    license="MIT",
+    version="0.2.1",
+    packages=setuptools.find_packages(),
+    author="Skuzzy xD",
+    author_email="techdroidroot@gmail.com",
+    description="A simple package made using Python and requests to get proxies from multiple sites!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="proxyscrape proxies proxygrab proxygrab-python proxylist",
+    url="http://github.com/SkuzzyxD/ProxyGrab/",
+    install_requires=requirements,
+    python_requires=">=3.6",
+    entry_points="""
+        [console_scripts]
+        proxygrab=proxygrab.cmdline:clicmd""",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+)
